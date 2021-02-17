1bb8432a68b1f89e3e27872fff48be34f95f6915
Raphael Freitas <raphael@hawks.com.br>
2021-02-17 15:24:57 -0300

    fist commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,54 @@
-import Head from 'next/head'
+import { useState } from "react";
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [isLight, setLightTheme] = useState(true);
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      style={{
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "flex-end",
+        backgroundColor: isLight ? "#fff" : "#222",
+        backgroundImage: isLight ? "none" : `url('/focus_mode.gif')`,
+        backgroundSize: "contain",
+        backgroundRepeat: "repeat",
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          top: 50,
+          right: 50,
+        }}
+      >
+        <button
+          role="button"
+          onClick={() => setLightTheme(!isLight)}
+          className={styles.button}
+          style={{
+            backgroundColor: isLight ? "#fff" : "red",
+            color: isLight ? "blue" : 'white'
+          }}
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+          {isLight ? "Focus Mode" : "Calm Mode"}
+        </button>
+      </div>
+      <div
+        style={{
+          backgroundImage: isLight
+            ? `url('/nunes_ultimo-gas.png')`
+            : `url('/nunes_ultimo-gas_whith_evil-eyes.png')`,
+          backgroundPosition: "center center",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "contain",
+          width: 700,
+          height: '80vh',
+        }}
+      />
     </div>
-  )
+  );
 }
